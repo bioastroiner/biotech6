5f4f936d332483326e9ecd807fcc28bbb0f434b1
Bioastroiner <naderi81eco@gmail.com>
2023-01-08 18:17:49 +0330

    Make WM stuff work

--- src/main/java/biotech/BioTech6Mod.java
+++ src/main/java/biotech/BioTech6Mod.java
@@ -1,10 +1,22 @@
 package biotech;
 
+import com.wildmobsmod.items.WildMobsModItems;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.*;
 import gregapi.api.Abstract_Proxy;
 import gregapi.code.ModData;
+import gregapi.data.CS;
+import gregapi.data.FL;
+import gregapi.data.MT;
+import gregapi.data.RM;
+import gregapi.oredict.OreDictItemData;
+import gregapi.util.OM;
+import gregapi.util.ST;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+import static gregapi.data.CS.*;
 
 @Mod(modid = BioTech6Mod.MOD_ID, name = BioTech6Mod.MOD_NAME, version = BioTech6Mod.VERSION, dependencies = "required-after:gregapi_post")
 public final class BioTech6Mod extends gregapi.api.Abstract_Mod {
@@ -14,8 +26,17 @@ public final class BioTech6Mod extends gregapi.api.Abstract_Mod {
     public static final String VERSION = Tags.VERSION;
     public static ModData MOD_DATA = new ModData(MOD_ID, MOD_NAME);
 
+    public static final ModData MOD_WM = new ModData("wildmobsmod", "Wild Mobs");
+
+    @Mod.Instance
+    public static BioTech6Mod INSTANCE;
+
     @SidedProxy(modId = MOD_ID, clientSide = Tags.CLIENTSIDE, serverSide = Tags.SERVERSIDE)
     public static Abstract_Proxy PROXY;
+
+    public BioTech6Mod(){
+        INSTANCE = this;
+    }
 
     @Override
     public String getModID() {
@@ -75,17 +96,116 @@ public final class BioTech6Mod extends gregapi.api.Abstract_Mod {
 
     @Override
     public void onModPreInit2(FMLPreInitializationEvent aEvent) {
-        new Compat_Recipes_WildMobs(new ModData("wildmobs", "Wild Mobs"), this);
+        OUT.println("BioTech_Mod: PreInit");
+        EventHandler.init();
+        //MinecraftForge.EVENT_BUS.register(this);
+        //MinecraftForge.EVENT_BUS.register(EventHandler.class);
+        //new Compat_Recipes_WildMobs(new ModData("wildmobsmod", "Wild Mobs"), this);
+
+//        new OreDictListenerEvent_Names() {
+//            @Override
+//            public void addAllListeners() {
+////                addListener("gemCertusQuartz", new IOreDictListenerEvent() {
+////                    @Override
+////                    public void onOreRegistration(OreDictRegistrationContainer aEvent) {
+////                        RM.ae_grinder(10, aEvent.mStack, OP.dust.mat(MT.CertusQuartz, 1));
+////                    }
+////                });
+//            }
+//        };
+
+
+        /************************************************************************************************************************/
+        ItemStack test1 = ST.make(MOD_WM,"venison",1);
+        ItemStack test2 = ST.make(WildMobsModItems.rawVenison,1,W);
+
+        CS.FoodsGT.put(ST.make(MOD_WM, "venison", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_venison", 1), 0, 0, 0, 10, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "calamari", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_calamari", 1), 0, 0, 0, 10, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "bison_meat", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_bison_meat", 1), 0, 0, 0, 10, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "chevon", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_chevon", 1), 0, 0, 0, 10, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "goose", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_goose", 1), 0, 0, 0, 10, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "mouse", 1), 0, 0, 0, 0, 12);
+        CS.FoodsGT.put(ST.make(MOD_WM, "cooked_mouse", 1), 0, 0, 0, 10, 12);
+
+        OUT.println("BioTech_Mod: Registring WildMobs OreDicts.");
+        OM.reg(ST.make(MOD_WM, "venison", 1), "foodVensionraw");
+        OM.reg(ST.make(MOD_WM, "venison", 1), "listAllvensionraw");
+        OM.reg(ST.make(MOD_WM, "chevon", 1), "listAllmuttonraw");
+        OM.reg(ST.make(MOD_WM, "cooked_venison", 1), "foodVensioncooked");
+        OM.reg(ST.make(MOD_WM, "cooked_venison", 1), "listAllvensioncooked");
+        OM.reg(ST.make(MOD_WM, "cooked_chevon", 1), "listAllmuttoncooked");
+
+        OM.reg(ST.make(MOD_WM, "calamari", 1), "foodCalamariraw");
+        OM.reg(ST.make(MOD_WM, "calamari", 1), "listAllcalamariraw");
+        OM.reg(ST.make(MOD_WM, "cooked_calamari", 1), "foodCalamaricooked");
+        OM.reg(ST.make(MOD_WM, "cooked_calamari", 1), "listAllcalamaricooked");
+
+        OM.reg(ST.make(MOD_WM, "bison_meat", 1), "listAllbeef");
+        OM.reg(ST.make(MOD_WM, "cooked_bison_meat", 1), "listAllcookedbeef"); // TODO: check this
+        OM.reg(ST.make(MOD_WM, "bison_meat", 1), "listAllbison");
+        OM.reg(ST.make(MOD_WM, "cooked_bison_meat", 1), "listAllcookedbison");
+
+        OM.reg(ST.make(MOD_WM, "bison_meat", 1), "listAllmeatraw");
+        OM.reg(ST.make(MOD_WM, "calamari", 1), "listAllmeatraw");
+        OM.reg(ST.make(MOD_WM, "chevon", 1), "listAllmeatraw");
+        OM.reg(ST.make(MOD_WM, "goose", 1), "listAllmeatraw");
+        OM.reg(ST.make(MOD_WM, "mouse", 1), "listAllmeatraw");
+        OM.reg(ST.make(MOD_WM, "venison", 1), "listAllmeatraw");
+
+        OM.reg(ST.make(MOD_WM, "cooked_bison_meat", 1), "listAllmeatcooked");
+        OM.reg(ST.make(MOD_WM, "cooked_calamari", 1), "listAllmeatcooked");
+        OM.reg(ST.make(MOD_WM, "cooked_chevon", 1), "listAllmeatcooked");
+        OM.reg(ST.make(MOD_WM, "cooked_goose", 1), "listAllmeatcooked");
+        OM.reg(ST.make(MOD_WM, "cooked_mouse", 1), "listAllmeatcooked");
+        OM.reg(ST.make(MOD_WM, "cooked_venison", 1), "listAllmeatcooked");
+
+        OUT.println("BioTech_Mod: Recipes for WildMobs Items.");
+        RM.Mixer.addRecipe1(T, 16, 16, ST.make(MOD_WM, "fur", 1), FL.Water.make(1000), NF, ST.make(Items.leather, 1, W));
+        RM.Mixer.addRecipe1(T, 16, 16, ST.make(MOD_WM, "bison_leather", 1), FL.Water.make(1000), NF, ST.make(Items.leather, 1, W));
+        RM.Mixer.addRecipe1(T, 16, 16, ST.make(MOD_WM, "fur", 1), FL.Water.make(1000), NF, ST.make(Items.leather, 1, W));
+        RM.Generifier.addRecipe1(T, 16, 16, ST.make(MOD_WM, "infected_flesh", 1), NF, NF, ST.make(Items.rotten_flesh, 1, W));
+
+        RM.Shredder.addRecipe1(T, 16, 16, ST.make(MOD_WM, "armadillo_shell", 1), OM.dust(MT.Ca, U / 9));
+        RM.Shredder.addRecipe1(T, 16, 16, ST.make(MOD_WM, "thick_bone", 1), OM.dust(MT.Bone, U * 10));
+
+        OM.data(ST.make(MOD_WM, "bison_leather", 1), new OreDictItemData(MT.Leather, U));
+        OM.data(ST.make(MOD_WM, "fur", 1), new OreDictItemData(MT.Leather, U));
+        OM.data(ST.make(MOD_WM, "armadillo_shell", 1), new OreDictItemData(MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "thick_bone", 1), new OreDictItemData(MT.Bone, U * 10));
+        OM.data(ST.make(MOD_WM, "venison", 1), new OreDictItemData(MT.MeatRaw, U * 3, MT.Bone, U2));
+        OM.data(ST.make(MOD_WM, "infected_flesh", 1), new OreDictItemData(MT.MeatRotten, U * 2, MT.Bone, U9));
+
+        //OM.data(ST.make(WM, "slimeDrop", 1), new OreDictItemData(MT.SlimyBone, U * 10));
+
+        OM.data(ST.make(MOD_WM, "cooked_bison_meat", 1), new OreDictItemData(MT.MeatCooked, U * 4, MT.Bone, U4));
+        OM.data(ST.make(MOD_WM, "cooked_calamari", 1), new OreDictItemData(MT.FishCooked, U * 2));
+        OM.data(ST.make(MOD_WM, "cooked_chevon", 1), new OreDictItemData(MT.MeatCooked, U * 3, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "cooked_goose", 1), new OreDictItemData(MT.MeatCooked, U * 3, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "cooked_mouse", 1), new OreDictItemData(MT.MeatCooked, U * 1, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "cooked_venison", 1), new OreDictItemData(MT.MeatCooked, U * 3, MT.Bone, U9));
+
+        OM.data(ST.make(MOD_WM, "bison_meat", 1), new OreDictItemData(MT.MeatRaw, U * 3, MT.Bone, U4));
+        OM.data(ST.make(MOD_WM, "calamari", 1), new OreDictItemData(MT.FishRaw, U * 2));
+        OM.data(ST.make(MOD_WM, "chevon", 1), new OreDictItemData(MT.MeatRaw, U * 3, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "goose", 1), new OreDictItemData(MT.MeatRaw, U * 3, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "mouse", 1), new OreDictItemData(MT.MeatRaw, U2, MT.Bone, U9));
+        OM.data(ST.make(MOD_WM, "venison", 1), new OreDictItemData(MT.MeatRaw, U * 3, MT.Bone, U9));
+        /************************************************************************************************************************/
     }
 
     @Override
     public void onModInit2(FMLInitializationEvent aEvent) {
-
+        OUT.println("BioTech_Mod: Init");
     }
 
     @Override
     public void onModPostInit2(FMLPostInitializationEvent aEvent) {
-        // Insert your PostInit Code here and not above
+        OUT.println("BioTech_Mod: PostInit");
     }
 
     @Override
--- src/main/java/biotech/EventHandler.java
+++ src/main/java/biotech/EventHandler.java
@@ -1,5 +1,6 @@
 package biotech;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregapi.data.CS;
@@ -11,6 +12,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
 import javax.annotation.Nullable;
@@ -19,70 +21,88 @@ import java.util.List;
 import static gregapi.data.CS.*;
 
 public class EventHandler {
+
+    public static void init() {
+        EventHandler handler = new EventHandler();
+        FMLCommonHandler.instance().bus().register(handler);
+        MinecraftForge.EVENT_BUS.register(handler);
+    }
+
     public static void handleDrops(EntityLivingBase aDead, String aClass, List<EntityItem> aDrops, int aLooting, boolean aBurn, boolean aPlayerKill) {
         if (Loader.isModLoaded("wildmobsmod")) {
-            boolean RIB = F, DOG = F, BIG = F;
-            if ("Dread".equalsIgnoreCase(aClass)) {
-            } else if ("Faded".equalsIgnoreCase(aClass)) {
-            } else if ("MagmaPlant".equalsIgnoreCase(aClass)) {
-            } else if ("SeaScorpion".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "crayfishrawItem", 1), ST.make(MD.HaC, "crayfishcookedItem", 1));
-            } else if ("Tarantula".equalsIgnoreCase(aClass)) {
+            boolean RIB = F, DOG = F, BIG = F, SCR = T;
+            if ("EntityDread".equalsIgnoreCase(aClass)) {
+            } else if ("EntityFaded".equalsIgnoreCase(aClass)) {
+                SCR = F;
+            } else if ("EntityMagmaPlant".equalsIgnoreCase(aClass)) {
+                SCR = F;
+            } else if ("EntitySeaScorpion".equalsIgnoreCase(aClass)) {
+                SCR = F;
+                drop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "crayfishrawItem", 1), ST.make(MD.HaC, "crayfishcookedItem", 1));
+            } else if ("EntityTarantula".equalsIgnoreCase(aClass)) {
+                SCR = F;
                 // Spider
-            } else if ("Zomgus".equalsIgnoreCase(aClass)) {
+            } else if ("EntityZomgus".equalsIgnoreCase(aClass)) {
+                SCR = F;
                 // zombie mushroom
-            } else if ("Armadillo".equalsIgnoreCase(aClass)) {
+            } else if ("EntityArmadillo".equalsIgnoreCase(aClass)) {
 
-            } else if ("Bison".equalsIgnoreCase(aClass)) {
+            } else if ("EntityBison".equalsIgnoreCase(aClass)) {
                 RIB = T;
                 BIG = T;
-            } else if ("Butterfly".equalsIgnoreCase(aClass)) {
+            } else if ("EntityButterfly".equalsIgnoreCase(aClass)) {
+                SCR = F;
 
-            } else if ("Cheetah".equalsIgnoreCase(aClass)) {
+            } else if ("EntityCheetah".equalsIgnoreCase(aClass)) {
                 DOG = T;
-            } else if ("Cougar".equalsIgnoreCase(aClass)) {
+            } else if ("EntityCougar".equalsIgnoreCase(aClass)) {
                 DOG = T;
-            } else if ("Deer".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "venisonrawItem", 1), ST.make(MD.HaC, "venisoncookedItem", 1));
+            } else if ("EntityDeer".equalsIgnoreCase(aClass)) {
+                drop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "venisonrawItem", 1), ST.make(MD.HaC, "venisoncookedItem", 1));
                 RIB = T;
-            } else if ("DireWolf".equalsIgnoreCase(aClass)) {
+            } else if ("EntityDireWolf".equalsIgnoreCase(aClass)) {
                 DOG = T;
                 BIG = T;
-            } else if ("Dragonfly".equalsIgnoreCase(aClass)) {
-                
-            } else if ("Fox".equalsIgnoreCase(aClass)) {
+            } else if ("EntityDragonfly".equalsIgnoreCase(aClass)) {
+                SCR = F;
+            } else if ("EntityFox".equalsIgnoreCase(aClass)) {
                 DOG = T;
-            } else if ("Goat".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_Mutton_Cooked.get(1), IL.Food_Mutton_Raw.get(1));
-            } else if ("Hyena".equalsIgnoreCase(aClass)) {
+            } else if ("EntityGoat".equalsIgnoreCase(aClass)) {
+            } else if ("EntityHyena".equalsIgnoreCase(aClass)) {
                 DOG = T;
-            } else if ("Jellyfish".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "jellyfish", 1), CS.NI);
-            } else if ("Mouse".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_Cheese.get(1), null);
-            } else if ("WMOcelot".equalsIgnoreCase(aClass)) {
+            } else if ("EntityJellyfish".equalsIgnoreCase(aClass)) {
+                SCR = F;
+                drop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "jellyfish", 1));
+            } else if ("EntityMouse".equalsIgnoreCase(aClass)) {
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_Cheese.get(1));
+            } else if ("EntityWMOcelot".equalsIgnoreCase(aClass)) {
                 DOG = T;
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_DogMeat_Cooked.get(1), IL.Food_DogMeat_Raw.get(1));
-            } else if ("WMWolf".equalsIgnoreCase(aClass)) {
+            } else if ("EntityWMWolf".equalsIgnoreCase(aClass)) {
                 DOG = T;
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_DogMeat_Cooked.get(1), IL.Food_DogMeat_Raw.get(1));
-            } else if ("goose".equalsIgnoreCase(aClass)) {
-                burntDrop(aDead, aDrops, aLooting, aBurn, ST.make(MD.HaC, "turkeyrawItem", 1), ST.make(MD.HaC, "turkeycookedItem", 1)); // turkey = goose
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_Gooseberry.get(1), NI); // make sense
+            } else if ("EntityGoose".equalsIgnoreCase(aClass)) {
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_Gooseberry.get(1)); // make sense
             }
             int tAmount = 1;
-            if (BIG) tAmount += RNGSUS.nextInt(1);
+            if (BIG) tAmount += 1;
             if (DOG)
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_DogMeat_Cooked.get(tAmount), IL.Food_DogMeat_Raw.get(tAmount));
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_DogMeat_Raw.get(tAmount), IL.Food_DogMeat_Cooked.get(tAmount));
             if (RIB)
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_Rib_Raw.get(tAmount), IL.Food_Rib_Raw.get(tAmount));
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_Rib_Raw.get(tAmount), IL.Food_Rib_Cooked.get(tAmount));
             if (RIB)
-                burntDrop(aDead, aDrops, aLooting, aBurn, IL.Food_RibEyeSteak_Raw.get(tAmount), IL.Food_RibEyeSteak_Raw.get(tAmount));
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_RibEyeSteak_Raw.get(tAmount), IL.Food_RibEyeSteak_Cooked.get(tAmount));
+            if (SCR)
+                drop(aDead, aDrops, aLooting, aBurn, IL.Food_Scrap_Meat.get(tAmount));
         }
     }
 
-    private static void burntDrop(EntityLivingBase aDead, List<EntityItem> aDrops, int aLooting, boolean aBurn, ItemStack aRaw, @Nullable ItemStack aCooked) {
+    private static void drop(EntityLivingBase aDead, List<EntityItem> aDrops, int aLooting, boolean aBurn, ItemStack aRaw) {
+        drop(aDead, aDrops, aLooting, aBurn, aRaw, NI);
+    }
+
+    private static void drop(EntityLivingBase aDead, List<EntityItem> aDrops, int aLooting, boolean aBurn, ItemStack aRaw, @Nullable ItemStack aCooked) {
+        if (aRaw == NI) return;
         if (aCooked == CS.NI) aCooked = aRaw.copy();
+        if (aCooked == NI) return;
         int tAmount = RNGSUS.nextInt(3);
         if (aLooting > 0) tAmount += RNGSUS.nextInt(aLooting + 1);
         while (tAmount-- > 0) aDrops.add(ST.entity(aDead, aBurn ? aCooked : aRaw));
