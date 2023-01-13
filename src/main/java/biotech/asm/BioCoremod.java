557b001ba4baba2d28dfa54f6baf32885b2fdbbe
Bioastroiner <naderi81eco@gmail.com>
2023-01-13 17:45:11 +0330

    Enable Mixins

---
+++ src/main/java/biotech/asm/BioCoremod.java
@@ -0,0 +1,34 @@
+package biotech.asm;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+@IFMLLoadingPlugin.TransformerExclusions({"biotech.asm"})
+public class BioCoremod implements IFMLLoadingPlugin {
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return "BioTech Coremod";
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
